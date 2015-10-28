609eabbe3d240175f7e09e62b3e9f03a74ceb866
Andrew Morris <voltrevo@gmail.com>
2015-10-28 19:08:05 +1100

    Lint fixes

--- test/index.js
+++ test/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var assert = require('assert');
-var gracefulMotion = require('../lib');
+/* global describe it */
 
-describe('graceful-motion', function () {
-  it('should have unit test!', function () {
+var assert = require('assert');
+// var gracefulMotion = require('../lib');
+
+describe('graceful-motion', function() {
+  it('should have unit test!', function() {
     assert(false, 'we expected this package author to add actual unit tests.');
   });
 });
